312d9e6e7df817cafc4cd226ca5a79062bee14f3
lxm <1970191805@qq.com>
2018-09-12 16:06:04 +0800

    音乐播放器搭建成功

---
+++ src/components/MusicPlay.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './MusicPlay.css'
+
+export default class MusicPlay extends Component {
+  render() {
+    return (
+      <div className="music-box">
+       
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MusicPlay from './components/MusicPlay.js';
+
+ReactDOM.render(<MusicPlay />, document.getElementById('root'));
